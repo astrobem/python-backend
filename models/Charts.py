a4fdfce088e56237d60dfdbb96b20a44d985d2df
Lucas <57031937+lukaqueres@users.noreply.github.com>
2023-03-09 16:26:17 +0100

    Update Charts.py

--- models/Charts.py
+++ models/Charts.py
@@ -14,15 +14,18 @@ class MultiChart:
 	def four(title: str = None, **data):
 		keys = list(data.keys())
 		fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(20, 10), layout="constrained")
+		i = 0
 		for x in range(2):
 			for y in range(2):
-				axs[x, y].set_title(keys[x+y])
-				axs[x, y].plot(data[keys[x+y]], color='blue', marker='o', linestyle='dashed')
-				axs[x, y].set_ylabel(MultiChart.units[keys[x+y]], bbox=MultiChart.yLabelBox)
-				axs[x, y].annotate(keys[x+y], (0.5, 0.5),
+				axs[x, y].set_title(keys[i])
+				axs[x, y].plot(data[keys[i]], color='blue', marker='o', linestyle='dashed')
+				axs[x, y].set_ylabel(MultiChart.units[keys[i]] if keys[i] in list(MultiChart.units.keys()) else None, bbox=MultiChart.yLabelBox)
+				axs[x, y].annotate(keys[i], (0.5, 0.5),
 				                   transform=axs[x, y].transAxes,
 				                   ha='center', va='center', fontsize=18,
 				                   color='lightblue')
+				i += 1
+			i += 1
 		if title:
 			fig.suptitle(title)
 		plt.show()
