d5ba3425dbd3f53f65e126350a878efa6d411907
kochamlukasa <123631912+kochamlukasa@users.noreply.github.com>
2023-04-19 00:25:00 +0200

    progresito

---
+++ Final/discrod.py
@@ -0,0 +1,15 @@
+# This example requires the 'message_content' intent.
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.all()
+
+client = MyClient(intents=intents)
+client.run('MTA5ODAwNjIyNTk3NjgyNzkxNA.Gm9ZIZ.MqxnXvdvSKoro5RBmxa04JeFpBKRYWQWjNNreE')
